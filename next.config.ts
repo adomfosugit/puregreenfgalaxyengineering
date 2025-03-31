d04ab10760957badd29646cffc3de4718d08ab07
adomfosu2000 <adomfosugit@gmail.com>
2025-03-31 10:06:28 +0000

    builderros

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  eslint: {
 
-const nextConfig: NextConfig = {
-  /* config options here */
+ ignoreDuringBuilds: true,
+},
+
+  typescript: {
+ ignoreBuildErrors: true,
+},
+
+   images:{
+     remotePatterns:[
+       {
+         protocol: 'https',
+         hostname: 'cloud.appwrite.io',
+         port: '',
+         pathname: '/v1/storage/buckets/**',
+       },
+     ]
+   }
 };
 
 export default nextConfig;
