0191977da05479fe375cede471c15aa19a8e4a54
adomfosu2000 <adomfosu2000@gmail.com>
2025-04-30 11:42:15 +0000

    happy now

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
   description: "Inventory Management System",
 };
 
-//export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 export default async function RootLayout({children}: Readonly<{children: React.ReactNode;}>) 
 {
  
